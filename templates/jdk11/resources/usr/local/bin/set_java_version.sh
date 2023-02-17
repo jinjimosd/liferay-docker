1f61bf55f4aceb678f032edaadd18c15519c0285
jinjimosd <truongblx26031999@gmail.com>
2023-02-17 20:20:19 +0700

    liferay 11

--- templates/jdk11/resources/usr/local/bin/set_java_version.sh
+++ templates/jdk11/resources/usr/local/bin/set_java_version.sh
@@ -13,13 +13,13 @@ function main {
 		if [[ ! -e "/usr/lib/jvm/${JAVA_VERSION}" ]]
 		then
 			local architecture=$(dpkg --print-architecture)
-			local zulu_version=$(echo "${JAVA_VERSION}" | tr -dc '0-9')
+			local java_version=$(echo "${JAVA_VERSION}" | tr -dc '0-9')
 
-			create_symlink "${architecture}" "zulu-${zulu_version}"
-			update-java-alternatives -s zulu-"${zulu_version}"-"${architecture}"
+			create_symlink "${architecture}" "java-${java_version}"
+			update-java-alternatives -s java-"${java_version}"-"${architecture}"
 		fi
 
-		local zulu_jdks=$(ls /usr/lib/jvm/ | grep "zulu-.*-.*" | awk -F- '{print $1$2}' | paste -s -d "," | sed "s/,/, /g")
+		local java_jdks=$(ls /usr/lib/jvm/ | grep "jdk-.*" | awk -F- '{print $1$2}' | paste -s -d "," | sed "s/,/, /g")
 
 		if [ -e "/usr/lib/jvm/${JAVA_VERSION}" ]
 		then
@@ -27,10 +27,10 @@ function main {
 			PATH=/usr/lib/jvm/${JAVA_VERSION}/bin/:${PATH}
 
 			echo "[LIFERAY] Using ${JAVA_VERSION} JDK. You can use another JDK by setting the \"JAVA_VERSION\" environment variable."
-			echo "[LIFERAY] Available JDKs: ${zulu_jdks}."
+			echo "[LIFERAY] Available JDKs: ${java_jdks}."
 		else
 			echo "[LIFERAY] \"${JAVA_VERSION}\" JDK is not available in this Docker image."
-			echo "[LIFERAY] Available JDKs: ${zulu_jdks}."
+			echo "[LIFERAY] Available JDKs: ${java_jdks}."
 
 			exit 1
 		fi
